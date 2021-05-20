074bba76ecea644786009e41fe73a5f9cfbad32b
jadessoriano <jadelestersoriano@gmail.com>
2021-05-20 22:17:24 +0800

    add due attribute to payment

--- app/Http/Livewire/PatientComponent/PatientViewComponent.php
+++ app/Http/Livewire/PatientComponent/PatientViewComponent.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Livewire\PatientComponent;
 
+use App\Models\Patient;
 use Livewire\Component;
 
 class PatientViewComponent extends Component
 {
-    public function render()
-    {
-        return view('livewire.patient-component.patient-view-component');
+    public function render() { return 
+        view('livewire.patient-component.patient-view-component', [
+            'patients' => $this->rows
+        ]);
+    }
+
+    public function getRowsProperty() { return
+        Patient::with(['user', 'user.profile', 'payments'])->latest()->get();
     }
 }
