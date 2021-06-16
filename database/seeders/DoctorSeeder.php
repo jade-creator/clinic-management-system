0d85f3621ec1fbd35d91345bb9a839ff5281768e
jadessoriano <jadelestersoriano@gmail.com>
2021-06-17 05:04:03 +0800

    Users Seeder v3

--- database/seeders/DoctorSeeder.php
+++ database/seeders/DoctorSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\Doctor;
+//use App\Models\Doctor;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class DoctorSeeder extends Seeder
@@ -14,6 +17,42 @@ class DoctorSeeder extends Seeder
      */
     public function run()
     {
-        Doctor::factory()->count(5)->create();
+        //Doctor::factory()->count(1)->create();
+        DB::table('users')->delete();
+        
+        $doctors = [
+            [
+                'name' => 'Doctor',
+                'email' => 'doctor@gmail.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('qwert@1Q'), // password
+                'remember_token' => Str::random(10),
+                'role_id' => 2,
+                'created_at' => now(), 
+                'updated_at' => now()
+            ],
+            [
+                'name' => $this->faker->name(),
+                'email' => $this->faker->unique()->safeEmail(),
+                'email_verified_at' => now(),
+                'password' => Hash::make('qwert@1Q'), // password
+                'remember_token' => Str::random(10),
+                'role_id' => 2,
+                'created_at' => now(), 
+                'updated_at' => now()
+            ],
+            [
+                'name' => $this->faker->name(),
+                'email' => $this->faker->unique()->safeEmail(),
+                'email_verified_at' => now(),
+                'password' => Hash::make('qwert@1Q'), // password
+                'remember_token' => Str::random(10),
+                'role_id' => 2,
+                'created_at' => now(), 
+                'updated_at' => now()
+            ]
+        ];
+
+        DB::table('users')->insert($doctors);
     }
 }
