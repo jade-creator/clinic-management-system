bb49450d21544caff753e6f0a572a6381cbe4397
jadessoriano <jadelestersoriano@gmail.com>
2021-07-23 20:28:39 +0800

    coreui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,8 @@
         <script src="{{ asset('js/app.js') }}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/perfect-scrollbar.min.js" integrity="sha512-yUNtg0k40IvRQNR20bJ4oH6QeQ/mgs9Lsa6V+3qxTj58u2r+JiAYOhOW0o+ijuMmqCtCEg7LZRA+T4t84/ayVA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://unpkg.com/@popperjs/core@2"></script>
-        <script src="https://unpkg.com/@coreui/coreui/dist/js/coreui.bundle.min.js"></script>
+        {{-- <script src="https://unpkg.com/@coreui/coreui/dist/js/coreui.bundle.min.js"></script> --}}
+        <script src="https://unpkg.com/@coreui/coreui/dist/js/coreui.min.js"></script>
         {{-- <script src="https://unpkg.com/@coreui/coreui@3.4.0/dist/js/coreui.min.js"></script> --}}
         <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
         <script>
